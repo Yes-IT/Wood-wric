934e58fc3de0a6ae91287c4c6a06098e405a8c47
Yes-IT <47417122+Yes-IT@users.noreply.github.com>
2024-10-08 16:24:14 +0530

    Update config.js

--- public/admin/config.js
+++ public/admin/config.js
@@ -800,57 +800,37 @@ const config = {
         },
       ],
     },
+  {
+  label: "Get Started",
+  name: "get-started",
+  folder: "src/content/get-started",
+  create: true,
+  editor: { preview: false },
+  fields: [
+    { label: "Title", name: "title", widget: "string", required: true },
     {
-      label: "Get Started",
-      name: "get-started",
-      folder: "src/content/get-started",
-      create: true,
-      editor: { preview: false },
+      label: "Description",
+      name: "description",
+      widget: "text",
+      required: true,
+    },
+    {
+      label: "Steps",
+      name: "steps",
+      widget: "list",
       fields: [
         { label: "Title", name: "title", widget: "string", required: true },
+        { label: "Image", name: "image", widget: "image", required: true },
         {
-          label: "Description",
-          name: "description",
-          widget: "text",
-          required: true,
-        },
-        {
-          label: "Steps",
-          name: "steps",
-          widget: "list",
-          fields: [
-            { label: "Title", name: "title", widget: "string", required: true },
-            { label: "Image", name: "image", widget: "image", required: true },
-            {
-              label: "Description",
-              name: "description",
-              widget: "object",
-              fields: [
-                {
-                  label: "Text",
-                  name: "text",
-                  widget: "text",
-                  required: true,
-                },
-                {
-                  label: "Points",
-                  name: "points",
-                  widget: "list",
-                  fields: [
-                    {
-                      label: "Point",
-                      name: "point",
-                      widget: "text",
-                      required: true,
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    },
+          label: "Content",
+          name: "content",
+          widget: "markdown", // or use "text" with editor preview false for plain text input
+          required: true
+        }
+      ]
+    }
+  ]
+},
     {
       label: "Why Wood",
       name: "why-wood",

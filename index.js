bbd8a41920328a45182b2fdccd05fe1cc20c6f41
Yes-IT <47417122+Yes-IT@users.noreply.github.com>
2024-10-10 15:10:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 
 // Configure Nunjucks to load templates from the 'templates' folder
-nunjucks.configure('templates', { autoescape: true });
+nunjucks.configure('layouts', { autoescape: true });
 
 // Load the front matter YAML from the Markdown file
-const frontMatter = yaml.load(fs.readFileSync('content/file.md', 'utf8'));
+const frontMatter = yaml.load(fs.readFileSync('content/social-media/social-media.md', 'utf8'));
 
 // Render the Nunjucks template with the front matter data
-const output = nunjucks.render('template.njk', frontMatter);
+const output = nunjucks.render('partials/footer.njk', frontMatter);
 
 // Output the result (you could also write it to a file)
 console.log(output);

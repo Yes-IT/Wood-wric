899d68c5e157b5720dc594ee559738b89f56816d
Yes-IT <47417122+Yes-IT@users.noreply.github.com>
2024-10-16 12:28:27 +0530

    Update getSocialMedia.js

--- src/lib/getSocialMedia.js
+++ src/lib/getSocialMedia.js
@@ -4,7 +4,7 @@ const fm = require('front-matter');
 
 // Function to fetch social media data from markdown file
 function getSocialMedia() {
-  const filePath = path.join(__dirname, '../content/social-media.md');
+  const filePath = path.join(__dirname, '../content/social-media/social-media.md');
   const fileContent = fs.readFileSync(filePath, 'utf8');
   const { attributes } = fm(fileContent); // Parses the front matter
   return attributes; // Return the key-value pairs directly

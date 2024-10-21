af027b5d7e1d925d56563105990c6951a40b9442
Yes-IT <47417122+Yes-IT@users.noreply.github.com>
2024-10-21 14:00:05 +0530

    Update build.js

--- build.js
+++ build.js
@@ -12,56 +12,130 @@ const marked = require('marked');
 const { DateTime } = require("luxon");
 
 
-
-// Configuration object for site metadata
+//config object used to store data for some plugins used below and passed to  metalsmith as metadata for use in templates
 const siteConfig = {
     seoSuffix: " | WIRC",
     siteUrl: "https://youwood.com",
     siteName: "WIRC",
 };
 
-// Configuration for Nunjucks template options
 const templateConfig = {
     engineOptions: {
         autoescape: false,
         trimBlocks: true,
         lstripBlocks: true,
         filters: {
-            modifiedTime: (milliseconds) => new Date(milliseconds).toUTCString(),
-            cleanUrl: (val) => val.replace(/(?<!:)(\/\/)/gm, '/'),
-            sortEvents: (arr) => {
-                const now = DateTime.local();
-                return arr
-                    .filter(el => el.date > now)
-                    .sort((a, b) => a.date - b.date);
+            modifiedTime: function(milliseconds){
+                return new Date(milliseconds).toUTCString();
             },
-            sortCareers: (arr) => arr.sort((a, b) => a.position.localeCompare(b.position)),
-            md: (content) => content ? marked.parse(content) : '',
-            findByField: (val, field, collection) => {
-                if (!val || !field || !collection) return "";
-                return collection.find(item => item[field] === val);
+            cleanUrl: function(val){
+                const regex = /(?<!:)(\/\/)/gm;
+                const result = val.replace(regex, '/');
+                return result;
             },
-            getRelatedArticlesByIds: (ids, collection) => collection.filter(item => ids.includes(item.id)),
-            getRelatedArticles: (collection, collections, path) => {
-                const relatedArticles = collections['blog'];
-                if (collection.length > 1) {
-                    const type = collection.filter(c => c !== 'blog')[0];
-                    if (type) {
-                        return collections[type].filter(i => i.path !== path).slice(0, 3);
+            sortEvents: function(arr){
+                var now = DateTime.local();
+
+                //filter out past dates
+                arr.filter(function(el){
+                    var eventDate = el.date;
+                    return eventDate > now;
+                })
+                //sort
+                arr.sort(function(a,b){
+                    // var aDate = parseInt(a.date.replace(/-/g,''));
+                    // var bDate = parseInt(b.date.replace(/-/g,''));
+                    var aDate = a.date;
+                    var bDate = b.date;
+
+                    if(aDate < bDate){
+                        return -1;
+                    }
+                    if(aDate > bDate){
+                        return 1;
+                    }
+                    return 0;
+                })
+
+                return arr;
+            },
+            sortCareers: function(arr){
+                arr.sort(function(a,b){
+                    if(a.position < b.position){
+                        return -1;
+                    }
+                    if(a.position > b.position){
+                        return 1;
+                    }
+                    return 0;
+                })
+
+                return arr;
+            },
+            md: function(content){
+                let out = '';
+            if(content){
+                out = marked.parse(content);
+            }
+            return out;
+            },
+             /**
+             * Find an item in collection by field value
+             * @param {*} val
+             * @param {string} field to search in collection
+             * @param {array} collection - metalsmith collection
+             */
+            findByField: function(val, field, collection) {
+                if (!val || !field || !collection) {
+                    return "";
+                }
+                let r = collection.find(item => item[field] === val);
+                return r;
+            },
+            getRelatedArticlesByIds: function(ids, collection){
+                const articles = collection.filter(item => {
+                    return ids.includes(item.id);
+                });
+                return articles;
+            },
+            getRelatedArticles: function(collection, collections, path){
+            var relatedArticles = collections['blog'];
+                if(collection.length > 1){
+                    // var type = collection.filter((c) => c !== 'blog')[0];
+                    var type = collection.filter(function(c) {
+                        return c !== 'blog';
+                    });
+                    if(type[0]){
+                        relatedArticles = collections[type[0]];
+                        relatedArticles = relatedArticles.filter(function(i){
+
+                            return i.path !== path;
+                        });
                     }
                 }
-                return relatedArticles.slice(0, 3);
+                return relatedArticles.slice(0,3);
             },
-            getExcerpt: (item) => item.excerpt || item.seo?.description || '',
-            toJson: (val) => {
-                let json = JSON.stringify(val);
-                return json.replace(/\\n/g, "\\\\n").replace(/"/g, '\\"');
+            getExcerpt : function(item){
+                var excerpt = item.excerpt;
+
+                if (!excerpt && item.seo) {
+                    excerpt = item.seo.description
+                }
+
+                return excerpt;
+
+            },
+            toJson: function(val){
+                var json =  JSON.stringify(val);
+                json = json.replace(/\\n/g, "\\\\n");
+                json = json.replace(/"/g, '\\"');
+                
+                return json;
             }
         }
     }
 };
 
-// Set up Metalsmith build
 const siteBuild = metalsmith(__dirname)
     .metadata({
         modified: new Date(),
@@ -73,68 +147,65 @@ const siteBuild = metalsmith(__dirname)
     .source("./src/content")
     .destination("./build/")
     .clean(true)
-    .use(collections({
-        blog: {
-            pattern: ["blog/*.md", "!blog/index.md"],
-            sortBy: "date",
-            reverse: true
-        },
-        eventsPage: {
-            pattern: "events/index.md"
-        },
-        quizzes: {
-            pattern: "quizzes/*.md",
-            refer: false
-        },
-        careers: {
-            pattern: ["careers/*.md", "!careers/index.md"],
-            refer: false
-        },
-        events: {
-            pattern: ["events/*.md", "!events/index.md"],
-            refer: false
-        }
-    }))
-    .use(pagination({
-        "collections.blog": {
-            perPage: 9,
-            layout: "blog.njk",
-            first: "blog/index.html",
-            path: "blog/:num/index.html",
-            pageMetadata: {
-                title: "Blog",
-                seo: { pageTitle: "Blog" },
+    .use(
+        collections({
+            blog: {
+                pattern: ["blog/*.md","!blog/index.md"],
+                sortBy: "date",
+                reverse: true
+            },
+            eventsPage:{
+                pattern: "events/index.md"
+            },
+            quizzes:{
+                pattern: "quizzes/*.md",
+                refer: false
+            },
+            careers:{
+                pattern: ["careers/*.md", "!careers/index.md"],
+                refer: false
+            },
+            events:{
+                pattern: ["events/*.md", "!events/index.md"],
+                refer: false
             }
-        }
-    }))
+        })
+    )
+    .use(
+        pagination({
+            "collections.blog": {
+                perPage: 9,
+                layout: "blog.njk",
+                first: "blog/index.html",
+                path: "blog/:num/index.html",
+                pageMetadata: {
+                    title: "Blog",
+                    seo: {pageTitle: "Blog"},
+                }
+            }
+        })
+    )
     .use(metadata({
         quizzes: { private: true },
         careers: { private: true },
         events: { private: true },
+        
     }))
     .use(industrialPrivate.plugin())
     .use(markdown())
     .use(permalinks())
-    .use(layouts(templateConfig));
-
-// Use debug UI in development mode
+    .use(layouts(templateConfig))
+    
 if (process.env.NODE_ENV === "dev") {
     siteBuild.use(debugUi.report());
 }
+    
+siteBuild.use(sitemap({ hostname: siteConfig.siteUrl, omitIndex: true, xslUrl:'/sitemap.xsl', frontmatterIgnore: 'exclude_from_sitemap' }))
+siteBuild.build(function(err, files) {
+	if (err) {
+	    console.log('!!!!!', err, files)
+	}
+	//console.log(files)
+	console.log("Metalsmith finished!")
+})
 
-// Generate sitemap
-siteBuild.use(sitemap({
-    hostname: siteConfig.siteUrl,
-    omitIndex: true,
-    xslUrl: '/sitemap.xsl',
-    frontmatterIgnore: 'exclude_from_sitemap'
-}));
-
-// Build the site
-siteBuild.build((err, files) => {
-    if (err) {
-        console.error('Error during build:', err);
-    } else {
-        console.log("Metalsmith finished!");
-    }
-});
